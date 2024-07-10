ee0f45e2c68eb95213fdf504229980e1810d7c59
AngeTenShi <megazombie06pro@gmail.com>
2024-07-10 17:26:16 +0200

    gitlab se deploie sur argocd

---
+++ bonus/scripts/deploy.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+CURRENT_PATH=$(pwd)
+#Checking if cluster exits
+if k3d cluster get p3 2>/dev/null; then
+  k3d cluster delete p3
+fi
+
+# initialize cluster, if not working, delete with `k3d cluster delete p3`
+echo "CREATING CLUSTER..."
+k3d cluster create p3
+
+# create namspaces and set argocd as default
+echo "CREATING NAMESPACES..."
+kubectl apply -f $CURRENT_PATH/../confs/namespaces.yaml
+kubectl config set-context --current --namespace=argocd
+
+# install argocd in argocd namespace
+echo "INSTALLING ARGOCD..."
+kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# loop because they cannot be awaited at first since they are not even created.
+set +e
+while true; do
+  if kubectl wait --for=condition=ready pods --all --timeout=600s 2>/dev/null; then
+    break
+  fi
+done
+set -e
+
+# get creds (https://stackoverflow.com/questions/68297354/what-is-the-default-password-of-argocd)
+echo "GETTING ARGOCD CREDS..."
+echo "argocd creds: (user: admin, password is: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d))"
+
+helm repo add gitlab https://charts.gitlab.io 2>/dev/null
+helm repo update
+helm install gitlab gitlab/gitlab -f $CURRENT_PATH/../confs/values.yaml --namespace gitlab
+
+
+kubectl apply -f $CURRENT_PATH/../confs/gitlab-project.yaml
+kubectl apply -f $CURRENT_PATH/../confs/helm-gitlab.yaml
+# 7 min to launch almost
+kubectl wait --for=condition=available deployment gitlab-webservice-default --namespace=gitlab --timeout=600s
+
+# CREATE THE REPO
+GITLAB_PASSWORD=$(kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -ojsonpath='{.data.password}' | base64 --decode ; echo)
+GITLAB_IP=$(kubectl get svc gitlab-webservice-default -n gitlab -ojsonpath='{.spec.clusterIP}')
+PERSONAL_ACCESS_TOKEN=$(curl --header "Content-Type: application/json" --request POST "http://$GITLAB_IP/api/v4/session?login=root&password=$GITLAB_PASSWORD" | jq -r '.private_token')
+PROJECT_NAME="anggonza-iot-p3"
+curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -X POST "$GITLAB_URL/api/v4/projects" --data "name=$PROJECT_NAME"
+sleep 60
+
+# Import GitHub repo to GitLab
+GITHUB_REPO="https://github.com/achansel/anggonza-iot-p3.git"
+echo "IMPORTING GITHUB REPO TO GITLAB..."
+curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -X POST "$GITLAB_URL/api/v4/projects/$PROJECT_NAME/import" --data "url=$GITHUB_REPO"
+
+
+GITLAB_REPO="$GITLAB_URL/$PROJECT_NAME.git"
+# add the app, gitlab auto sync will be done every 3 minutes (default config)
+argocd app create wilapp --repo $GITLAB_REPO --path . --dest-server 'https://kubernetes.default.svc' --dest-namespace dev --sync-policy auto --self-heal
+
+# same as the previous similar loop
+set +e
+while true; do
+  if kubectl wait --for=condition=available deployment playground --namespace=dev --timeout=600s 2>/dev/null; then
+    break
+  fi
+done
+set -e
+
+
+echo "Now forwarding app to port 8888 and argocd to port 8080, Ctrl+C twice to interrupt (first argo, then app)"
+
+# forward ports because ingress setup is not too funny because of default HTTPS of argocd-server
+kubectl port-forward svc/playground -n dev 8888:8888 & kubectl port-forward svc/argocd-server -n argocd 8080:443 --address 0.0.0.0 && fg
---
+++ bonus/scripts/install_deps_debian.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -e
+
+# FOR debian12
+sudo apt update
+
+# cURL
+sudo apt install -y curl
+
+#jq 
+
+sudo apt install -y jq
+
+# docker
+sudo apt install -y docker.io
+sudo adduser $USER docker
+
+# k3d
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
+
+# argocd
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 755 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+# helm
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+echo "success"
