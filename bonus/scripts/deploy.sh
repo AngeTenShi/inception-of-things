5b2bdb0bb8c6aba2663b3bc5f823eb26dd674cc1
Andrea C <achansel@student.42nice.fr>
2024-07-15 20:10:05 +0200

    gitlab now works, first api calls are done.

--- bonus/scripts/deploy.sh
+++ bonus/scripts/deploy.sh
@@ -8,7 +8,7 @@ fi
 
 # initialize cluster, if not working, delete with `k3d cluster delete p3`
 echo "CREATING CLUSTER..."
-k3d cluster create p3
+k3d cluster create --k3s-arg "--disable=traefik@server:*" p3
 
 # create namspaces and set argocd as default
 echo "CREATING NAMESPACES..."
@@ -35,23 +35,38 @@ echo "argocd creds: (user: admin, password is: $(kubectl -n argocd get secret ar
 
 kubectl apply -f $CURRENT_PATH/../confs/helm-gitlab.yaml 
 # 7 min to launch almost
-kubectl wait --for=condition=available deployment gitlab-webservice-default --namespace=gitlab --timeout=600s
+set +e
+while true; do
+  if kubectl wait --for=condition=available deployment gitlab-webservice-default --namespace=gitlab --timeout=600s 2>/dev/null; then
+    break
+  fi
+done
+set -e
+
 
 # CREATE THE REPO
 GITLAB_PASSWORD=$(kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -ojsonpath='{.data.password}' | base64 --decode ; echo)
-GITLAB_IP=$(kubectl get svc gitlab-webservice-default -n gitlab -ojsonpath='{.spec.clusterIP}')
-PERSONAL_ACCESS_TOKEN=$(curl --header "Content-Type: application/json" --request POST "http://$GITLAB_IP/api/v4/session?login=root&password=$GITLAB_PASSWORD" | jq -r '.private_token')
+GITLAB_IP=$(kubectl get svc gitlab-nginx-ingress-controller -n gitlab -ojsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+# remove insecure when not self-signed no more
+alias curl='curl --insecure --header "Host: gitlab.example.com"'
+
+GITLAB_TOKEN=$(curl --header "Content-Type: application/json" -X POST "https://$GITLAB_IP/oauth/token?grant_type=password&username=root&password=$GITLAB_PASSWORD" --insecure | jq -r '.access_token')
+
 PROJECT_NAME="anggonza-iot-p3"
-curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -X POST "$GITLAB_URL/api/v4/projects" --data "name=$PROJECT_NAME"
-sleep 60
+curl -v --header "Authorization: Bearer $GITLAB_TOKEN" -X POST "https://$GITLAB_IP/api/v4/projects" --data "name=$PROJECT_NAME"
+
+sleep 10
 
 # Import GitHub repo to GitLab
 GITHUB_REPO="https://github.com/achansel/anggonza-iot-p3.git"
 echo "IMPORTING GITHUB REPO TO GITLAB..."
-curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -X POST "$GITLAB_URL/api/v4/projects/$PROJECT_NAME/import" --data "url=$GITHUB_REPO"
+# TODO: FIX THIS: https://docs.gitlab.com/ee/api/import.html#import-repository-from-github --> maybe clone it then push?
+curl --header "Authorization: Bearer $GITLAB_TOKEN" -X POST "https://$GITLAB_IP/api/v4/projects/$PROJECT_NAME/import" --data "url=$GITHUB_REPO"
 
+# todo: add right host here, so that it forwards to the right host, also fix the certificate.
+GITLAB_REPO="https://$GITLAB_IP/$PROJECT_NAME.git"
 
-GITLAB_REPO="$GITLAB_URL/$PROJECT_NAME.git"
 # add the app, gitlab auto sync will be done every 3 minutes (default config)
 argocd app create wilapp --repo $GITLAB_REPO --path . --dest-server 'https://kubernetes.default.svc' --dest-namespace dev --sync-policy auto --self-heal
 
